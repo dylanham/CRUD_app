312f00363eb49b154c57a1baa4aee7d950454b4b
Dylan Ham <dylanham@gmail.com>
2015-02-05 13:02:36 -0700

    crud demo app

---
+++ app/controllers/blog_posts_controller.rb
@@ -0,0 +1,49 @@
+class BlogPostsController < ApplicationController
+  def index
+    @blog_posts = BlogPost.all
+  end
+
+  def show
+
+    @blog_post = BlogPost.find(params[:id])
+
+  end
+
+
+  def new
+    @blog_post = BlogPost.new
+  end
+
+  def create
+    blog_post = BlogPost.new(blog_post_params)
+    blog_post.save
+
+    redirect_to blog_posts_path
+  end
+
+  def edit
+    @blog_post = BlogPost.find(params[:id])
+  end
+
+  def update
+    @blog_post = BlogPost.find(params[:id])
+
+    if @blog_post.update(blog_post_params)
+      redirect_to blog_posts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    blog_post = BlogPost.find(params[:id])
+    blog_post.destroy
+    redirect_to blog_posts_path
+  end
+
+  private
+
+  def blog_post_params
+    params.require(:blog_post).permit(:title, :author, :content)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  root to: 'blog_posts#index'
+  resources :blog_posts
+end
